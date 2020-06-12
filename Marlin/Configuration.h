8efcb46e3614044fc37d0d6cf2ba4c17d27ae747
HolgerJ96 <66846687+HolgerJ96@users.noreply.github.com>
2020-06-12 22:56:46 +0200

    ender 3 fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -983,7 +983,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define MIN_PROBE_EDGE 27
+#define PROBING_MARGIN 27
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
